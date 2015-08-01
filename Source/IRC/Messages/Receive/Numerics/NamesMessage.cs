f28ab36788785c5d7fbbd14eb4ba789f84193c11
Cyral <cyral@pyratron.com>
2015-08-01 14:49:22 -0400

    More UI work

---
+++ Source/IRC/Messages/Receive/Numerics/NamesMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace Pyratron.PyraChat.IRC.Messages.Receive.Numerics
+{
+    /// <summary>
+    /// Names message (RPL_NAMES).
+    /// </summary>
+    /// <see cref="http://tools.ietf.org/html/rfc2812#section-3.2.5" />
+    public class NamesMessage : ReceivableMessage
+    {
+        public NamesMessage(Message msg) : base(msg)
+        {
+
+        }
+
+        public static bool CanProcess(Message msg)
+        {
+            return msg.Type == "353";
+        }
+    }
+}
