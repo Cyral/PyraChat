48bef6720e835c10fd2b26a9a6e89b863cbcb9b4
Cyral <cyral@pyratron.com>
2015-08-02 17:56:26 -0400

    Client Models

---
+++ Source/UI/Models/ChatLine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Pyratron.PyraChat.IRC;
+
+namespace Pyratron.PyraChat.UI.Models
+{
+    /// <summary>
+    /// Represents a line of the chat.
+    /// </summary>
+    public class ChatLine
+    {
+        /// <summary>
+        /// User who sent the message. Null if system message.
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Message text.
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// Time message was received.
+        /// </summary>
+        public DateTime Time { get; }
+
+        /// <summary>
+        /// Creates a new chat line from the specified user.
+        /// </summary>
+        public ChatLine(User user, string message)
+        {
+            User = user;
+            Message = message;
+            Time = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Creates a system chat line.
+        /// </summary>
+        public ChatLine(string message)
+        {
+            Message = message;
+            Time = DateTime.Now;
+        }
+    }
+}
---
+++ Source/UI/Models/ServerSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyratron.PyraChat.UI.Models
+{
+    /// <summary>
+    /// Represents settings for an IRC server.
+    /// </summary>
+    public class ServerSettings
+    {
+        /// <summary>
+        /// Hostname.
+        /// </summary>
+        public string Host { get; }
+
+        /// <summary>
+        /// Port.
+        /// </summary>
+        public int Port { get; }
+
+        /// <summary>
+        /// Automatically connect at startup or when disconnected.
+        /// </summary>
+        public bool AutoConnect { get; }
+
+        /// <summary>
+        /// Use an SSL connection.
+        /// </summary>
+        public bool UseSSL { get; }
+
+        /// <summary>
+        /// Accept invalid SSL certificates.
+        /// </summary>
+        public bool AcceptInvalidSSLCertificates { get; }
+    }
+}
