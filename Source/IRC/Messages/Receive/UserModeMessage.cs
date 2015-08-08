06c74679d1a3028763365d69fd2ee2d561e55068
Cyral <cyral@pyratron.com>
2015-08-08 09:24:04 -0400

    Support for multiple channel modes

--- Source/IRC/Messages/Receive/UserModeMessage.cs
+++ Source/IRC/Messages/Receive/UserModeMessage.cs
@@ -1,28 +1,27 @@
-﻿using System;
-using System.Linq;
-
-namespace Pyratron.PyraChat.IRC.Messages.Receive
+﻿namespace Pyratron.PyraChat.IRC.Messages.Receive
 {
     /// <summary>
     /// User mode message. (MODE)
     /// </summary>
-    /// <see cref="http://tools.ietf.org/html/rfc2812#section-3.2.1"/>
+    /// <see cref="http://tools.ietf.org/html/rfc2812#section-3.2.1" />
     public class UserModeMessage : ReceivableMessage
     {
         public User User => BaseMessage.User;
+        private string Modes => BaseMessage.Parameters[1];
 
         public UserModeMessage(Message msg) : base(msg)
         {
             if (msg.User.Nick == msg.Destination) //User and nick sent must match (RFC spec)
             {
-                for (var i = 1; i < msg.Parameters.Length; i++)
+                var add = false;
+                foreach (var character in msg.Parameters[1])
                 {
-                    var mode = msg.Parameters[i];
-                    if (string.IsNullOrWhiteSpace(mode)) continue;
-                    if (mode[0].Equals('+'))
-                        msg.User.AddMode(msg.Client, mode[1]);
-                    else if (mode[0].Equals('+'))
-                        msg.User.RemoveMode(msg.Client, mode[1]);
+                    if (character.Equals('+'))
+                        add = true;
+                    else if (character.Equals('+'))
+                        add = false;
+                    else if (add) msg.User.AddMode(msg.Client, character);
+                    else msg.User.RemoveMode(msg.Client, character);
                 }
                 msg.Client.OnUserMode(this);
             }
