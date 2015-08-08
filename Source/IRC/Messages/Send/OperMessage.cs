1d5042f12c16a10f691bf1c9488a12fe265cfe76
Cyral <cyral@pyratron.com>
2015-08-07 21:22:49 -0400

    OPER message

---
+++ Source/IRC/Messages/Send/OperMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Pyratron.PyraChat.IRC.Messages.Send
+{
+    /// <summary>
+    /// Oper message.
+    /// </summary>
+    /// <see cref="http://tools.ietf.org/html/rfc2812#section-3.1.4" />
+    public class OperMessage : SendableMessage
+    {
+        public string Name { get; }
+        public string Password { get; }
+
+        public OperMessage(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+
+        public void Send(StreamWriter writer, Client client)
+        {
+            writer.WriteLine($"NICK {Name} {Password}");
+        }
+    }
+}
