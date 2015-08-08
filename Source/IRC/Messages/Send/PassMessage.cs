39c8033e345daa6de0f38016b7dd0db0aa3cd32a
Cyral <cyral@pyratron.com>
2015-08-07 21:18:40 -0400

    PASS message.

---
+++ Source/IRC/Messages/Send/PassMessage.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace Pyratron.PyraChat.IRC.Messages.Send
+{
+    /// <summary>
+    /// Pass message.
+    /// </summary>
+    /// <see cref="http://tools.ietf.org/html/rfc2812#section-3.1.1" />
+    public class PassMessage : SendableMessage
+    {
+        /// <summary>
+        /// Connection password.
+        /// </summary>
+        public string Password { get; }
+
+        public PassMessage(string password)
+        {
+            Password = password;
+        }
+
+        public PassMessage(User user)
+        {
+            Password = user.Nick;
+        }
+
+        public void Send(StreamWriter writer, Client client)
+        {
+            writer.WriteLine($"PASS {Password}");
+        }
+    }
+}
